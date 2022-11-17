6e5e81f26dae1dbc90887bfed8e5612cb7a5233e
Jaykumar Halpati <rameshhalpati007@gmail.com>
2022-11-17 13:26:29 +0000

    Removed local JSON file

--- src/api.js
+++ src/api.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const apiData = require("./data.json");
 
 const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json(
-    apiData
-  );
+  res.json({
+    hello: "hi!"
+  });
 });
 
 app.use(`/.netlify/functions/api`, router);
